3ab321b7d51807a6d3bcf01536a875f7606dbd1a
Yuya <inoueyuya416@gmail.com>
2021-11-07 16:58:42 +0900

    ルーティング設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'contents/new'
-  get 'contents/index'
-  get 'contents/show'
-  get 'contents/create'
-  get 'contents/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'schedules#index'
+  resources :schedules
+  resources :contents
 end
