0c1519660e90ec530725e412ec88c5c8a4d6e762
Yuya <inoueyuya416@gmail.com>
2021-11-08 15:55:43 +0900

    ajaxでタイトル追加を実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,10 @@
 class ContentsController < ApplicationController
   def new
     @content = Content.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def index
@@ -10,8 +14,25 @@ class ContentsController < ApplicationController
   end
 
   def create
+    @content = Content.new(content_params)
+
+    respond_to do |format|
+      if @content.save
+        format.html
+        format.json
+        format.js
+      else
+        format.js { render :new }
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :media, :url)
   end
 end
