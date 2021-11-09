96f8493e36b3186a90c73b0464ac068c12f8d5d6
Yuya <inoueyuya416@gmail.com>
2021-11-09 11:48:05 +0900

    seedに現在の視聴リストを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
 [
-  ['王様ランキング', 'Amazonプライム', 'https://www.amazon.co.jp/dp/B09JBGF9W9']
-  ['ジャヒー様はくじけない！', 'Amazonプライム', 'https://www.amazon.co.jp/dp/B09BDY2V6P']
-  ['無職転生 ～異世界行ったら本気だす～', 'Amazonプライム', 'https://www.amazon.co.jp/dp/B08SHW97KR']
-  ['先輩がうざい後輩の話', 'Abema', 'https://abema.tv/video/title/115-83']
-  ['８６―エイティシックス―', 'Abema', 'https://abema.tv/video/title/25-163']
-  ['大正オトメ御伽話', 'Abema', 'https://abema.tv/video/title/189-35']
-  ['白い砂のアクアトープ', 'Abema', 'https://abema.tv/video/title/194-23']
-  ['ブルーピリオド', 'Netflix', 'https://www.netflix.com/title/81318842']
-].each do |title, media, url|
+  ['王様ランキング', 'Amazonプライム', 'https://www.amazon.co.jp/dp/B09JBGF9W9', 6],
+  ['ジャヒー様はくじけない！', 'Amazonプライム', 'https://www.amazon.co.jp/dp/B09BDY2V6P',5],
+  ['無職転生 ～異世界行ったら本気だす～', 'Amazonプライム', 'https://www.amazon.co.jp/dp/B08SHW97KR', 7],
+  ['先輩がうざい後輩の話', 'Abema', 'https://abema.tv/video/title/115-83', 7],
+  ['８６―エイティシックス―', 'Abema', 'https://abema.tv/video/title/25-163', 7],
+  ['大正オトメ御伽話', 'Abema', 'https://abema.tv/video/title/189-35', 6],
+  ['白い砂のアクアトープ', 'Abema', 'https://abema.tv/video/title/194-23', 5],
+  ['ブルーピリオド', 'Netflix', 'https://www.netflix.com/title/81318842', 7]
+].each do |title, media, url, stream|
   Content.create!(
-    { title: title, media: media, url: url }
+    { title: title, media: media, url: url, stream: stream }
   )
+end
