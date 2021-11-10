4233c5220c057b38247975195679949895314a48
Yuya <inoueyuya416@gmail.com>
2021-11-10 16:06:54 +0900

    streamのenumを月曜日からに変更

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,7 @@
 class Content < ApplicationRecord
   has_one :schedule, dependent: :destroy
 
-  enum stream: { default: 0, Sun: 1, Mon: 2, Tue: 3, Wed: 4, Thu: 5, Fri: 6, Sat: 7 }
+  enum stream: { default: 0, Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6, Sun: 7 }
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :media, presence: true, length: { maximum: 100 }
