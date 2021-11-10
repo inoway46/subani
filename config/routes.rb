13f0da1c1d42ca8a68e158c4a4a9811bc8c5c150
Yuya <inoueyuya416@gmail.com>
2021-11-10 20:01:08 +0900

    ログアウト設定の修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :schedules
   resources :contents
   resources :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
