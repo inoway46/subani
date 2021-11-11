4bae8c1cef927499155ecf23a2465415daceec66
Yuya <inoueyuya416@gmail.com>
2021-11-11 10:58:57 +0900

    デプロイ用に設定を編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => herokuapp.com, :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'subani.herokuapp.com', :protocol => 'https'}
 end
