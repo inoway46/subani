2a96c133ae325d8ef5aba3b9c038206d7cbd9f51
Yuya <inoueyuya416@gmail.com>
2021-11-27 14:33:14 +0900

    マスタ登録時に時間割の登録ステータスをtrueに変更

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,7 +2,7 @@ class ContentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @contents = current_user.contents.order(stream: :asc)
+    @contents = Content.all #current_user.contents.order(stream: :asc)
   end
 
   def show
@@ -45,7 +45,7 @@ class ContentsController < ApplicationController
       @master_ids = params[:content][:master_id]
       @master_ids.each do |master_id|
         master = Master.find(master_id.to_i)
-        @content = current_user.contents.create!(title: master.title, media: master.media, url: master.url, stream: master.stream)
+        @content = current_user.contents.create!(title: master.title, media: master.media, url: master.url, stream: master.stream, registered: true)
         current_user.schedules.create!(content_id: @content.id, day: @content.stream_i18n)
       end
       redirect_to contents_path, alert: "#{@master_ids.size}件のタイトルと時間割を登録しました"
@@ -89,7 +89,7 @@ class ContentsController < ApplicationController
   private
 
   def content_params
-    params.require(:content).permit(:title, :media, :url, :stream, :user_id, :master_id => [])
+    params.require(:content).permit(:title, :media, :url, :stream, :master_id => [])
   end
 
   def result
