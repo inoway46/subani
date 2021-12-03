33f223509883528befc027323fe8cb70d71ed7e0
Yuya <inoueyuya416@gmail.com>
2021-12-03 17:10:02 +0900

    Contentテーブルにuser_idを付与するマイグレーションファイルを削除

--- db/migrate/20211110083912_add_user_id_to_contents.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToContents < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :contents, :user, foreign_key: true
-  end
-end
--- db/migrate/20211127032515_remove_user_id_from_content.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserIdFromContent < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key :contents, :user
-    remove_reference :contents, :user, index: true
-  end
-end
