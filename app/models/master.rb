91560fa2a29fbb0dafbd9b4cfdd10f7c6b764ae1
Yuya <inoueyuya416@gmail.com>
2021-12-07 16:05:43 +0900

    MasterとContentにアソシエーションを追加 #25

--- app/models/master.rb
+++ app/models/master.rb
@@ -1,4 +1,6 @@
 class Master < ApplicationRecord
+  has_many :contents, dependent: :destroy
+
   enum stream: { default: 0, Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6, Sun: 7 }
   
   validates :title, presence: true, length: { maximum: 255 }
