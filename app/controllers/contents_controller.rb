f83596633f2f76fbd9ff99d0a65f5a16eacc42b5
Yuya <inoueyuya416@gmail.com>
2021-12-09 19:41:10 +0900

    マスタ登録時に最新話通知をデフォルトでオンに

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -35,7 +35,7 @@ class ContentsController < ApplicationController
       @master_ids = params[:content][:master_id]
       @master_ids.each do |master_id|
         master = Master.find(master_id.to_i)
-        @content = current_user.contents.create!(title: master.title, media: master.media, url: master.url, stream: master.stream, registered: true, episode: master.episode, master_id: master.id)
+        @content = current_user.contents.create!(title: master.title, media: master.media, url: master.url, stream: master.stream, registered: true, new_flag: true, episode: master.episode, master_id: master.id)
         current_user.schedules.create!(content_id: @content.id, day: @content.stream_i18n)
       end
       redirect_to contents_path, alert: "#{@master_ids.size}件のタイトルと時間割を登録しました"
