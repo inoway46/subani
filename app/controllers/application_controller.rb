8af162fe391e16c726524dc33938d69417cb45d2
Yuya <inoueyuya416@gmail.com>
2021-12-21 00:23:55 +0900

    今後の実装のためdeviseのパラメーター追加処理を追記 #35

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  #before_action :configure_permitted_parameters
   def after_sign_in_path_for(resource)
     schedules_path(resource)
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:line_nonce])
   end
 end
