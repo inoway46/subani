b5549b243ec936ce72e65d425496c55ebd867733
Yuya <inoueyuya416@gmail.com>
2021-12-27 18:49:03 +0900

    postback式でアカウント連携解除処理を実装 #35

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,9 +3,6 @@ class LineBotController < ApplicationController
 
   protect_from_forgery except: :callback
 
-  LINK = "アカウント連携"
-  UNLINK = "連携解除"
-
   def callback
     client = Line::Bot::Client.new do |config|
       config.channel_id = ENV["LINE_CHANNEL_ID"]
@@ -19,9 +16,9 @@ class LineBotController < ApplicationController
 
     events = client.parse_events_from(body)
     events.each do |event|
-      message = case event
-                when Line::Bot::Event::AccountLink
-                  if event.result == "ok"
+      case event
+        when Line::Bot::Event::AccountLink
+          message= if event.result == "ok"
                     @line_id = event['source']['userId']
                     @user = User.find_by(line_nonce: event.nonce.to_s)
                     if User.exists?(uid: @line_id)
@@ -30,15 +27,31 @@ class LineBotController < ApplicationController
                     else
                       @user.update!(uid: @line_id, line_nonce: nil)
                       reply_text("アカウントの連携が完了しました")
+                      #リッチメニューのリクエストを送る
                     end
                   else
                     reply_text("アカウントの連携に失敗しました")
                   end
-                when Line::Bot::Event::Message
-                  { type: 'text', text: parse_message_type(event) }
-                else
-                  { type: 'text', text: '........' }
-                end
+        when Line::Bot::Event::Postback
+          #連携解除で「はい」を選択
+          case event['postback']['data']
+          when "confirm"
+            @user = User.find_by(uid: event['source']['userId'])
+            if @user.present?
+              @user.update!(uid: nil)
+              message = reply_text("LINEアカウントの連携を解除しました")
+            else
+              message = reply_text("アカウントは連携されていません")
+            end
+          #連携解除で「いいえ」を選択
+          when "cancel"
+            message = reply_text("引き続きサブスク通知をお楽しみください")
+          end
+        when Line::Bot::Event::Message
+          message = { type: 'text', text: parse_message_type(event) }
+        else
+          message = { type: 'text', text: '........' }
+        end
       client.reply_message(event['replyToken'], message)
     end
     head :ok
@@ -51,7 +64,7 @@ class LineBotController < ApplicationController
     when Line::Bot::Event::MessageType::Text
       reaction_text(event)   # ユーザーが投稿したものがテキストメッセージだった場合に返す値
     else
-      'ありがとうございます'             # ユーザーが投稿したものがテキストメッセージ以外だった場合に返す値
+      'ありがとうございます'   # ユーザーが投稿したものがテキストメッセージ以外だった場合に返す値
     end
   end
 
@@ -65,25 +78,37 @@ class LineBotController < ApplicationController
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     end
 
-    #アカウント連携
-    if event.message['text'].match?(LINK)
+    case event.message['text']
+    when "アカウント連携"
       userid = event['source']['userId']
       response = client.create_link_token(userid).body
       link_token = JSON.parse(response)
-      uri = URI("https://subani.herokuapp.com/line/link")
+      uri = URI("https://9af4-2001-ce8-131-389f-509d-a7a3-feac-9e24.ngrok.io/line/link")
       uri.query = URI.encode_www_form({ linkToken: link_token["linkToken"] })
       "下記のリンクよりログインしてアカウント連携を行ってください。\n#{uri}"
-    #連携解除
-    elsif event.message['text'].match?(UNLINK)
-        @user = User.find_by(uid: event['source']['userId'])
-        if @user.present?
-          @user.update!(uid: nil)
-          "LINEアカウントの連携を解除しました"
-        else
-          "アカウントは連携されていません"
-        end
-    else
-      event.message['text']
+    when "連携解除"
+      message = {
+        "type": "template",
+        "altText": "This is a buttons template",
+        "template": {
+            "type": "buttons",
+            "title": "連携解除",
+            "text": "アカウント連携を解除しますか？",
+            "actions": [
+                {
+                  "type": "postback",
+                  "label": "はい",
+                  "data": "confirm"
+                },
+                {
+                  "type": "postback",
+                  "label": "いいえ",
+                  "data": "cancel"
+                }
+            ]
+        }
+      }
+      client.reply_message(event['replyToken'], message)
     end
   end
 
