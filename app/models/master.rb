e5f4847ca4c95db26c353aba6222cb62a4c32a16
Yuya <inoueyuya416@gmail.com>
2022-01-02 21:19:13 +0900

    Masterモデルにscopeを追加、Dayモジュールをinclude #41

--- app/models/master.rb
+++ app/models/master.rb
@@ -1,4 +1,5 @@
 class Master < ApplicationRecord
+  include Day
   has_many :contents, dependent: :destroy
 
   enum stream: { default: 0, Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6, Sun: 7 }
@@ -7,4 +8,9 @@ class Master < ApplicationRecord
   validates :media, presence: true, length: { maximum: 100 }
   validates :url, presence: true, length: { maximum: 2048 }
   validates :stream, presence: true
+
+  scope :abema_titles, -> { where(media: "Abemaビデオ") }
+  scope :amazon_titles, -> { where(media: "Amazonプライム") }
+  scope :now_streaming, -> { where(season: "now") }
+  scope :today, -> { where(update_day: day_of_week) }
 end
