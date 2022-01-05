f365d5f2978b07af1b0092f4ef7ed35a28ffbfb9
Yuya <inoueyuya416@gmail.com>
2022-01-05 11:45:01 +0900

    Safariのログアウト問題の解消 #46

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     schedules_path(resource)
