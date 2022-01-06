0cfaba7d6bc8d41791f7bf068e77c6f83b9d3203
Yuya <inoueyuya416@gmail.com>
2022-01-06 15:47:10 +0900

    herokuのDBエラー解消

--- db/migrate/20220106041056_create_line_notifications.rb
+++ db/migrate/20220106041056_create_line_notifications.rb
@@ -1,6 +1,7 @@
 class CreateLineNotifications < ActiveRecord::Migration[5.2]
   def change
     create_table :line_notifications do |t|
+      t.references :user, foreign_key: true
       t.references :content, foreign_key: true
 
       t.timestamps
