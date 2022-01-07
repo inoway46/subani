506c0d428736ea481f3bf3fcae98e3424086ddb0
Yuya <inoueyuya416@gmail.com>
2022-01-07 17:44:11 +0900

    Fix: contentモデルの不要なアソシエーションを削除 #26

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,7 +3,6 @@ class Content < ApplicationRecord
   has_many :user_contents, dependent: :destroy
   has_many :users, through: :user_contents
   has_many :line_flags, dependent: :destroy
-  has_many :line_notifications, dependent: :destroy
   belongs_to :master
 
   enum stream: { default: 0, Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6, Sun: 7 }
