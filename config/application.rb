32a2897fae391e242221a42fde6f4d369f17830e
Yuya <inoueyuya416@gmail.com>
2022-01-07 16:58:09 +0900

    generatorの設定を変更 #26

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,19 @@ module Subani
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.decorator   false
+      g.skip_routes true
+      g.system_tests = nil
+      g.test_framework :rspec,
+        fixtures: true,
+        controller_specs: false,
+        view_specs: false, 
+        helper_specs: false,
+        routing_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
