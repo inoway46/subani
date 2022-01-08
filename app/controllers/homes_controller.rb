b72b3d9b3266540b9aabad2e68038fc5e2aae591
Yuya <inoueyuya416@gmail.com>
2022-01-08 11:54:37 +0900

    プロフィールページに退会ボタンを追加、ゲストユーザーのデフォルトネームを設定 #51

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,10 +3,9 @@ class HomesController < ApplicationController
   end
 
   def guest_sign_in
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザー') do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
     sign_in user
     redirect_to schedules_path, notice: 'ゲストユーザーとしてログインしました。'
