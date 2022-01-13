bfcdb44977101ebb7fdc43381e5192d4e0a6aac0
Yuya <inoueyuya416@gmail.com>
2022-01-13 15:08:29 +0900

    メタタグの設定 #52

--- Gemfile
+++ Gemfile
@@ -86,6 +86,9 @@ gem 'draper'
 #LINE通知数のカウント
 gem 'counter_culture'
 
+#OGPの設定
+gem 'meta-tags'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'サブスクアニメ時間割',
+      title: 'サブスクアニメ時間割',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Amazon、Abema、Netflixの今期アニメを時間割形式で管理。アニメの最新話をLINEで通知します。',
+      keywords: '今期アニメ,スケジュール,サブスク,Amazonプライム,ABEMA,Netflix',
+      canonical: request.original_url,
+      separator: '|',
+      noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'サブスクアニメ時間割',
+        title: 'サブスクアニメ時間割',
+        description: 'Amazon、Abema、Netflixの今期アニメを時間割形式で管理。アニメの最新話をLINEで通知します。',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@y_waic',
+      }
+    }
+  end
 end
