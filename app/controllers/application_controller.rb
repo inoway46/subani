65de674fb9c08697b087bc7265baaa435cea5f44
Yuya <inoueyuya416@gmail.com>
2022-01-16 17:01:08 +0900

    独自ドメインへのリダイレクト処理を追加 #52

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :ensure_domain, if: :production?
+  FQDN = 'subani.net'
+
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
+
   def after_sign_in_path_for(resource)
     contents_path(resource)
   end
@@ -16,5 +25,9 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:uid])
+  end
+
+  def production?
+    Rails.env.production?
   end
 end
