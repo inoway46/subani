381eb540aeb4eeb6f28716b4c6d877de2ddea103
Yuya <inoueyuya416@gmail.com>
2022-11-13 16:47:31 +0900

    Add rpecの設定とテストファイルのひな形を追加

---
+++ spec/lib/tasks/master_csv_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'master_csv' do
+  describe 'import_with_line' do
+    subject(:task) { Rake.application['import_with_line'] }
+
+    # it 'import_with_line' do
+    #   expect { task.invoke }.to
+    # end
+  end
+end
