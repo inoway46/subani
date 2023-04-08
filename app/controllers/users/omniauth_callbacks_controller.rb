25249b555f1137b13e815c86f44fb07898e5ce8e
Yuya <inoueyuya416@gmail.com>
2023-04-08 16:40:45 +0900

    不要なメソッドの削除、リファクタ

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,20 +7,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def basic_action
     @omniauth = request.env["omniauth.auth"]
-    if @omniauth.present?
-      @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
-      if @profile.email.blank?
-        email = @omniauth["info"]["email"] ? @omniauth["info"]["email"] : "#{@omniauth["uid"]}-#{@omniauth["provider"]}@example.com"
-        @profile = current_user || User.create!(provider: @omniauth["provider"], uid: @omniauth["uid"], email:, name: @omniauth["info"]["name"], password: Devise.friendly_token[0, 20])
-      end
-      @profile.set_values(@omniauth)
-      sign_in(:user, @profile)
+    return if @omniauth.blank?
+
+    @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
+    return if @profile.provider.to_s != @omniauth["provider"].to_s || @profile.uid != @omniauth["uid"]
+
+    if @profile.email.blank?
+      email = @omniauth["info"]["email"] || "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
+      @profile = current_user || User.create!(provider: @omniauth["provider"], uid: @omniauth["uid"], email:, name: @omniauth["info"]["name"], password: Devise.friendly_token[0, 20])
     end
+    sign_in(:user, @profile)
     flash[:notice] = t('.basic_action.success')
     redirect_to contents_path
   end
 
-  def fake_email(uid, provider)
+  def fake_email(_uid, _provider)
     "#{auth.uid}-#{auth.provider}@example.com"
   end
 end
